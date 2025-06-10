978643d10496424f1e403b63e06ac1aa969f8888
Tanya Gray <tanyagray@Tanyas-MacBook-Air.local>
2025-06-10 23:33:42 +0100

    update app

--- symantix/src/App.js
+++ symantix/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -6,12 +6,8 @@ import Divider from '@mui/material/Divider';
 import AppTheme from './shared-theme/AppTheme';
 import AppAppBar from './components/AppAppBar';
 import Hero from './components/Hero';
-import LogoCollection from './components/LogoCollection';
 import Highlights from './components/Highlights';
-import Pricing from './components/Pricing';
 import Features from './components/Features';
-import Testimonials from './components/Testimonials';
-import FAQ from './components/FAQ';
 import Footer from './components/Footer';
 
 
@@ -19,7 +15,6 @@ function App() {
   return (
     <div>
    
-
       <AppTheme>
       <CssBaseline enableColorScheme />
       <AppAppBar />
